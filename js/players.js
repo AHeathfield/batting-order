8f06c820aaf96bc28321ff82329155cc95c6f47e
Aidan Heathfield <aidan.heathfield@gmail.com>
2024-08-07 23:53:12 -0400

    update

---
+++ js/players.js
@@ -0,0 +1,21 @@
+const PLAYERS = {
+    "Aidan Heathfield":     "male",
+    "Alexis Coe":           "female",
+    "Andreas Ponvia":       "male",
+    "Andrew Caton":         "male",
+    "Brandon Gallant":      "male",
+    "Brittany Lauton":      "female",
+    "Bruce Ross":           "male",
+    "Chris Heathfield":     "male",
+    "Emily Matuszewski":    "female",
+    "Heather Gelinas":      "female",
+    "Jacob Heathfield":     "male",
+    "Katarina":             "female",
+    "Mr Black":             "male",
+    "Robert Mule":          "male",
+    "Samantha":             "female",
+    "Tom Norris":           "male",
+    "Tracey Prokipczuk":    "female"
+};
+
+//For now use obj entries to go through this
